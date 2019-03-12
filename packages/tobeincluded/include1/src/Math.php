d979b69590d915addc272c4d640799fcc24e13c3
hs040x <hsitohang@wayfair.com>
2019-03-12 11:42:46 +0100

    add includes

---
+++ packages/tobeincluded/include1/src/Math.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author    Harry Osmar Sitohang <hsitohang@wayfair.com>
+ * @copyright 2019 Wayfair LLC - All rights reserved
+ */
+
+namespace Package\Include1;
+
+class Math {
+  public function multiply(float $param1, float $param2) : float {
+    return $param1 * $param2;
+  }
+}
---
+++ packages/tobeincluded/include2/src/Greet.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author    Harry Osmar Sitohang <hsitohang@wayfair.com>
+ * @copyright 2019 Wayfair LLC - All rights reserved
+ */
+
+namespace Package\Include2;
+
+class Greet {
+  public function sayHello() : string {
+    return 'hello world';
+  }
+}
